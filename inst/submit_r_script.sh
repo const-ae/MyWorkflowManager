1f11b0f32a1beee5b7b85f4039db7cb24da8719c
const-ae <artjom31415@googlemail.com>
2023-03-22 16:01:52 +0100

    Initial commit

---
+++ inst/submit_r_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# error handler function, modified from
+# https://stackoverflow.com/a/50265513
+
+exit_if_error() {
+  local exit_code=$1
+  shift
+  [[ $exit_code ]] &&               # do nothing if no error code passed
+    ((exit_code != 0)) && {         # do nothing if error code is 0
+      printf 'ERROR: %s\n' "$@" >&2 # we can use better logging here
+      exit "$exit_code"
+    }
+    printf '%s finished succesfully\n' "$@" # success message
+}
+
+echo "Start Script"
+
+module load R-bundle-Bioconductor/3.15-foss-2021b-R-4.2.0
+STAT_FILE=$1
+shift
+/usr/bin/time --output=$STAT_FILE --format="elapsed %e\nuser %U\nsys %S\nmax_mem_kbytes %M\n" Rscript --no-restore $@
+exit_if_error $? "Rscript"
+
+
+echo "Finished Script"
